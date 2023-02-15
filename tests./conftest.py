ef6425c17e733ebe7c844d652e9f0f41e9e1dbd4
Omerta933 <80603765+Omerta933@users.noreply.github.com>
2023-02-15 18:56:22 -0500

    Update conftest.py

--- tests./conftest.py
+++ tests./conftest.py
@@ -1,11 +1,9 @@
-
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as chrome_options
 from selenium.webdriver.support.event_firing_webdriver import EventFiringWebDriver
 
 from abstract.selenium_listener import MyListener
-
 
 @pytest.fixture
 def get_chrome_options():
@@ -15,15 +13,13 @@ def get_chrome_options():
     options.add_argument('--window-size=1650,900')
     return option
 
-
 @pytest.fixture
 def get_webdriver(get_chrome_options):
     options = get_chrome_options
     driver = webdriver.Chrome(options=options)
     return driver
   
-  
-@pytest.fixture(scope='function')
+  @pytest.fixture(scope='function')
 def setup(request, get_webdriver):
     driver = get_webdriver
     driver = EventFiringWebDriver(driver, MyListener())
